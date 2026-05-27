888a7adc5c2d86318da3643d9f88dc19a4639667
igorao2802-dev <igorao2802@gmail.com>
2026-05-27 23:49:37 +0300

    del_some_files

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="app-footer">
-      <p>© 2026 Салон «Здоровье и красота». Все права защищены.</p>
-      <p>Система онлайн-записи. Версия 1.0</p>
-    </footer>
-  );
-}
